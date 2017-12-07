203801711109de4d94c6dcd0f16fdc066840ecf9
insayd10 <b2rky48@gmail.com>
2017-12-08 00:17:04 +0200

    Language insertion -> Chine
    Simplified Chinese support for PHP-DS usage has been added.

---
+++ PHP-DS/lang/ZH.php
@@ -0,0 +1,23 @@
+<?php
+$dil= 
+[
+#Supported Class Name
+"a_class"				=>		"PHP_DS_SUPPORTED",
+#Supported Variables
+"$1"					=>		"类", // class determiner
+"$2"					=>		"运用", // using determiner
+"$3"					=>		"功能", // function determiner
+"$4"					=>		"变量",  //variable determiner
+"$5"					=>		"码", // code determiner
+"$6"					=>		"文件", // file determiner
+"$7"					=>		"将支持", //will_support determiner
+"$8"					=>		"类", // class lang
+"$9"					=>		"静态的", // static lang
+"$10"					=>		"功能",  // function lang
+"$11"					=>		"变量", // variable lang
+"$12"					=>		"语言支持不能在没有语言文件的情况下提供。",//Language Support can not be provided without a language file.
+"$13"					=>		"未找到指定的语言文件。",// The specified language file was not found
+"$14"					=>		"函数名称无效。",// Function name is invalid
+#Supported Functions
+"F1"					=>		"加" // Add() function's lang
+];
